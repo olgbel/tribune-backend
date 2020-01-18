26898e6fcf31d3b552b376d553d0b67b398d457d
OlgaKC <olgbelous@gmail.com>
2020-01-19 00:03:32 +0300

    drop sout lines

--- src/main/kotlin/ru/netology/route/RoutingV1.kt
+++ src/main/kotlin/ru/netology/route/RoutingV1.kt
@@ -64,7 +64,6 @@ class RoutingV1(
                     route("/update/user"){
                         put {
                             val input = call.receive<UserRequestDto>()
-                            println("input: $input")
                             val response = userService.update(input)
                             call.respond(response)
                         }
